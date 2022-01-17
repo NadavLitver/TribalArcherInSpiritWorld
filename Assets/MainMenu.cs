3db8c9210583576dda5e2a54b7792d1be3a4fcd1
Amir Aharoni <65896568+TheWize@users.noreply.github.com>
2022-01-17 19:28:35 +0200

    Sounds upload, menu script, ps_release 0.1

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float ButtonPressDelay = 0.2f;
+    public void Button_Start()
     {
-        
+        StartCoroutine(StartRoutine());
     }
-
-    // Update is called once per frame
-    void Update()
+    private IEnumerator StartRoutine()
     {
-        
+        StopAllCoroutines();
+        yield return new WaitForSeconds(ButtonPressDelay);
+        SceneManager.LoadScene(1);
+    }
+    public void Button_Exit()
+    {
+        StartCoroutine(ExitRoutine());
+    }
+    private IEnumerator ExitRoutine()
+    {
+        StopAllCoroutines();
+        yield return new WaitForSeconds(ButtonPressDelay);
+        Application.Quit();
     }
 }
