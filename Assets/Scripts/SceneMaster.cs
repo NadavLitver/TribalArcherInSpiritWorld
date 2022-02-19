36698b0fcdbd767f192e0eb3d50d78e84d2fa072
Nadav Litver <Nlitver@gmail.com>
2022-02-19 13:06:23 +0200

    scene fixes

---
+++ Assets/Scripts/SceneMaster.cs
@@ -0,0 +1,77 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneMaster : MonoBehaviour
+{
+    public static SceneMaster instance;
+    //[FoldoutGroup("Refrences")]
+    //public Slider slider;
+    //[FoldoutGroup("Refrences")]
+    //public TextMeshProUGUI text;
+    //[SerializeField, FoldoutGroup("Refrences")]
+    //private GameObject loadingScreen;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        //loadingScreen.SetActive(false);
+    }
+    private void Start()
+    {
+    }
+
+    public void LoadLevel(int index)
+    {
+        StartCoroutine(LoadAsync(index));
+    }
+    IEnumerator LoadAsync(int index)
+    {
+        //loadingScreen.SetActive(true);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        while (!asyncOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            //slider.value = progress;
+            //text.text = (progress * 100f) + "%";
+            yield return null;
+        }
+        //loadingScreen.SetActive(false);
+
+    }
+    public void LoadLevel(string scene)
+    {
+        StartCoroutine(LoadAsync(scene));
+    }
+    IEnumerator LoadAsync(string scene)
+    {
+        //loadingScreen.SetActive(true);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene);
+        while (!asyncOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            //slider.value = progress;
+            //text.text = (progress * 100f) + "%";
+            yield return null;
+        }
+        //loadingScreen.SetActive(false);
+
+    }
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+}
