e4a5370074516fe444f88711336b7fb1c6de494e
Nadav Litver <Nlitver@gmail.com>
2022-05-22 10:39:18 +0300

    health orb fixes

--- Assets/Scripts/FlyingEnemyBody.cs
+++ Assets/Scripts/FlyingEnemyBody.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class FlyingEnemyBody : Livebody
 {
+    [FoldoutGroup("Properties"), Range(0, 100),SerializeField]
+    float chanceToDropHealthOrb;
     protected override void SummonDeadBody()
     {
-        base.SummonDeadBody();
+        float ran = Randomizer.ReturnRandomFloat(new Vector2(0,100));
+        if (HealthOrb != null && ran > chanceToDropHealthOrb)
+        {
+           Instantiate(HealthOrb, CenterPivot.position, Quaternion.identity, null);
+        }
         SoundManager.Play(SoundManager.Sound.OwlDead, audioSource);
         //VFXManager.Play(VFXManager.Effect.FlyingEnemyDead, CenterPivot.position);
     }
--- Assets/Scripts/SuicideBomberLivebody.cs
+++ Assets/Scripts/SuicideBomberLivebody.cs
@@ -1,12 +1,20 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SuicideBomberLivebody : Livebody
 {
+    [FoldoutGroup("Properties"), Range(0, 100), SerializeField]
+    float chanceToDropHealthOrb;
+    
     protected override void SummonDeadBody()
     {
-        base.SummonDeadBody();
+        float ran = Randomizer.ReturnRandomFloat(new Vector2(0, 100));
+        if (HealthOrb != null && ran > chanceToDropHealthOrb)
+        {
+            Instantiate(HealthOrb, CenterPivot.position, Quaternion.identity, null);
+        }
         SoundManager.Play(SoundManager.Sound.OwlDead, audioSource);
     }
     public override void TakeDamage(int damage)
